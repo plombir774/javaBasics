afb7cec7d962ebf7f91af1bbee52e0adf01febef
plombir774 <plombir774@gmail.com>
2026-03-30 22:30:03 +0500

    добавил allure

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,17 +1,34 @@
 package tests;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.testng.annotations.BeforeMethod;
 import pages.LoginPage;
 
 import static com.codeborne.selenide.Selenide.open;
 
-public class BaseTest {
-  public  LoginPage loginPage = new LoginPage();
-    @BeforeMethod
-    public void BasicLogin(){
-        open("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-        loginPage.Creds("Admin","admin123");
 
 
-    }
-}
+        public class BaseTest {
+
+            public LoginPage loginPage = new LoginPage();
+
+            @BeforeMethod
+            public void setUp() {
+
+                SelenideLogger.addListener("AllureSelenide",
+                        new AllureSelenide()
+                                .screenshots(true)
+                                .savePageSource(true));
+
+
+                open("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+
+                loginPage.Creds("Admin", "admin123");
+            }
+        }
+
+
+
+
